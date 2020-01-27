274a1a192a3b2bfe4c1928fd602b3e822b4dde07
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-01-27 08:09:14 +0000

    static folder added with style.css

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def insert_task():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=(os.environ.get('PORT')),
+            debug=True)
